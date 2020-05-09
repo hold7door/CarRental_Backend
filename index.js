db7aa81576a1612b6240d29f7a8e8652625e7b7a
hold7door <arpitpathak97@gmail.com>
2020-05-09 10:59:30 +0530

    Dotenv FIx

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 //Database Setup for MongoDb Atlas
 var db_user = process.env.DB_USER;
